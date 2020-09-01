d2177c921ddcecd7df1d414b239dc8ff92eeda63
Madushan47 <61668132+Madushan47@users.noreply.github.com>
2020-09-01 17:50:50 +0530

    model for projects
    Created a model class for projects/Posts

---
+++ Raise-up_web/src/main/java/com/raiseup/models/Project.java
@@ -0,0 +1,92 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.raiseup.models;
+
+/**
+ *
+ * @author Madushan
+ */
+public class Project {
+    
+    private int id;
+    private String title;
+    private String description;
+    private String organizer;
+    private String target;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+
+    /**
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the organizer
+     */
+    public String getOrganizer() {
+        return organizer;
+    }
+
+    /**
+     * @param organizer the organizer to set
+     */
+    public void setOrganizer(String organizer) {
+        this.organizer = organizer;
+    }
+
+    /**
+     * @return the target
+     */
+    public String getTarget() {
+        return target;
+    }
+
+    /**
+     * @param target the target to set
+     */
+    public void setTarget(String target) {
+        this.target = target;
+    }
+    
+    
+    
+}
